666bf52af68b81a28d04307b7692b934546f4345
the 14th web <dennis.itua1@gmail.com>
2019-10-22 03:38:42 -0400

    updating js and firebase details

---
+++ assets/javascript/firebase.js
@@ -0,0 +1,52 @@
+// alert("working")
+// $
+
+var config = {
+    apiKey: "AIzaSyBw07HQpEJAG77e0xe_-eW2rjJIhlRo0Zo",
+    authDomain: "my-portfolio-92fc8.firebaseapp.com",
+    databaseURL: "https://my-portfolio-92fc8.firebaseio.com",
+    projectId: "my-portfolio-92fc8",
+    storageBucket: "my-portfolio-92fc8.appspot.com",
+    messagingSenderId: "255672623201",
+    appId: "1:255672623201:web:b21b821fa673a9ec918a20",
+    measurementId: "G-ECZXRJNT05"
+  };
+
+firebase.initializeApp(config);
+
+var messagesRef = firebase.database().ref("messages");
+
+document.getElementById("contactform").addEventListener("submit",
+ submitForm);
+
+function submitForm(){
+// e.preventDefault();
+// console.log(123);
+var name = getInputVal("name");
+var phone = getInputVal("phone");
+var email = getInputVal("email");
+var message = getInputVal("message");
+//  var name= $("#Form-Input1").val();
+//   var email= $("#Form-Input2").val();
+//    var message= $("#Text-area1").val();
+// console.log(name);
+
+saveMessage(name, phone, email, message);
+
+alert("Your message has been sent!");
+
+}
+
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+function saveMessage(name, phone, email, message){
+var newMessageRef = messagesRef.push();
+newMessageRef.set({
+name: name,
+phone: phone,
+email: email,
+message: message
+});
+}
