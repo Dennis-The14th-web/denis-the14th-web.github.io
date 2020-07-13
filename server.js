8bb3baaf7322585c2d7d3fefd541e352a35fb3c2
the 14th web <dennis.itua1@gmail.com>
2020-07-13 04:32:21 -0400

    updating more details

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Database configuration
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("public"));
+}
 // DB Setup (connect mongoose and instance of mongodb)
 var databaseUrl = mongoose.connect(process.env.MONGOLAB || "mongodb://portfoliocoding:portfoliocodingmaster1$@ds161517.mlab.com:61517/heroku_d10n8ht4");
 var collections = ["feedbacks"];
