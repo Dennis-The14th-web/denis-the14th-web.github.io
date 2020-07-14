5b1f2757b9752610bcdc2021d260858f0396ed9d
the 14th web <dennis.itua1@gmail.com>
2020-07-14 11:31:43 -0400

    updating server.js details

--- server.js
+++ server.js
@@ -68,5 +68,5 @@ app.post("/submit", (req, res) => {
 const port = process.env.PORT || 3000;
 // const server = http.createServer(app);
 app.listen(port);
-console.log('Server listening on: ', port);
+console.log("Server listening on:", port);
 
