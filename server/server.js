7e7937b8c329906fdf97626d9bc5ff4f9a8dc067
louis mane <dennis.itua1@gmail.com>
2021-03-16 18:52:47 -0400

    updated EC2 AWS

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const http = require('http'); 
 const path = require('path');
+const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 require('./models/Post');
 const Feedback = mongoose.model('Feedback');
@@ -13,16 +15,25 @@ const { MONGODB_URI } = require('./config/keys');
 // Initialize express
 const app = express();
 
+mongoose.connect(MONGODB_URI,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true
+    })
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.error(err));
+
 // Access middleware
-app.use(bodyParser.json());
-// app.use('/submit', postsRoutes);
-
-// Accessing home page
-app.use(express.static("public"));
-
 // Data parsing
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+
+// CORS Middleware
+app.use(cors());
 
 // post route
 app.post('/', (req, res) => {
@@ -44,29 +55,19 @@ app.post('/', (req, res) => {
         (err)
     })
 });
-mongoose.connect(MONGODB_URI,
-{ useNewUrlParser: true,
-useUnifiedTopology: true 
-})
-mongoose.connection.on('connected',()=>{
-    console.log("conneted to db")
-})
-mongoose.connection.on('error',(err)=>{
-    console.log("err connecting",err)
-})
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
     });
   }
 
 // module.exports = router;
 
 // Listen to port and spin server
-const Port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 5000;
-const server = http.createServer(app);
-server.listen(Port);
-console.log('Server now listening on PORT', Port)
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+  console.log(`Server Running on Port: ${port}`)
+});
